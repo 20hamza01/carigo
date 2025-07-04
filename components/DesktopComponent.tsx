6b45497b4738f35d4fcae12372dcc8dcc2cb5acf
20hamza01 <90298569+20hamza01@users.noreply.github.com>
2025-07-04 21:04:08 +0100

    Adding Animation

--- components/DesktopComponent.tsx
+++ components/DesktopComponent.tsx
@@ -8,6 +8,7 @@ import LuxuriousCars from "@/components/LuxuriousCars";
 import MidClassCars from "@/components/MidClassCars";
 import EconomicCars from "@/components/EconomicCars";
 import { useTranslations } from "next-intl";
+import { motion } from "framer-motion";
 
 const DesktopComponent = () => {
   const tSupport = useTranslations("support");
@@ -24,38 +25,64 @@ const DesktopComponent = () => {
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-blue-800 via-blue-500 to-blue-200 relative overflow-hidden">
-        <div className="rounded-full bg-white/9 absolute h-[700px] w-[700px] right-[-100px] top-[-100px]"></div>
-        <div className="rounded-full bg-white/9 absolute h-[700px] w-[700px] left-[-350px] top-160"></div>
-        <Link
-          href={whatsappLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="animate-pulse fixed bottom-10 right-6 bg-green-500 z-590 hover:bg-green-600 text-white p-2 rounded-full shadow-lg transition-all duration-300 flex items-center justify-center"
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
         >
-          <Image
-            width={100}
-            height={100}
-            src="/whatsapp-icon.png"
-            alt="WhatsApp"
-            className="w-10 h-10"
-          />
-        </Link>
-        <div className="relative z-10">
-          <Navbar />
-        </div>
+          <div className="rounded-full bg-white/9 absolute h-[700px] w-[700px] right-[-100px] top-[-100px]"></div>
+          <div className="rounded-full bg-white/9 absolute h-[700px] w-[700px] left-[-350px] top-160"></div>
+          <Link
+            href={whatsappLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="animate-pulse fixed bottom-10 right-6 bg-green-500 z-590 hover:bg-green-600 text-white p-2 rounded-full shadow-lg transition-all duration-300 flex items-center justify-center"
+          >
+            <Image
+              width={100}
+              height={100}
+              src="/whatsapp-icon.png"
+              alt="WhatsApp"
+              className="w-10 h-10"
+            />
+          </Link>
+          <div className="relative z-10">
+            <Navbar />
+          </div>
 
-        <Hero />
-
+          <Hero />
+        </motion.div>
         <section className="px-6 pt-16 pb-24 bg-white" id="cars">
-          <EconomicCars />
+          <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.6 }}
+          >
+            <EconomicCars />
 
-          <Separator className="mb-16 mt-32" />
+            <Separator className="mb-16 mt-32" />
+          </motion.div>
 
-          <MidClassCars />
+          <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.6 }}
+          >
+            <MidClassCars />
 
-          <Separator className="mb-16 mt-32" />
+            <Separator className="mb-16 mt-32" />
+          </motion.div>
 
-          <LuxuriousCars />
+          <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.6 }}
+          >
+            <LuxuriousCars />
+          </motion.div>
         </section>
       </div>
     </>
